5e68352db56610f6c67baf7caff864a0fefdfd6b
Joe Schurer <jwschurer@gmail.com>
2025-05-03 22:01:37 -0400

    fixing to get all on linux

--- sniffer_ip_header_decode.py
+++ sniffer_ip_header_decode.py
@@ -34,7 +34,7 @@ class IP:
 
 class ICMP:
     def __init__(self,buff):
-        header = struct.unpack('<BBHHH',buff)
+        header = struct.unpack('!BBHHH',buff)
         self.type = header[0]
         self.code = header[1]
         self.sum = header[2]
@@ -44,23 +44,25 @@ class ICMP:
 def sniff(host):
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
-    else:
-        socket_protocol =  socket.IPPROTO_ICMP
-    
-    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((host,0))
-    #grab ip header
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    if os.name == 'nt':
+        sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+        sniffer.bind((host, 0))
+        sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
         try:
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
         except OSError as e:
             print(f"Error enabling promiscuous mode: {e}")
             sys.exit(1)
+    else:
+        # Linux: use AF_PACKET to get all IP traffic
+        sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        sniffer.bind((host, 0))  # `host` is the interface name, e.g. 'eth0'
     
     try:
         while True:
             raw_buffer = sniffer.recvfrom(65535)[0]
+            if os.name != 'nt':
+                raw_buffer = raw_buffer[14:]  # skip Ethernet header
+
             ip_header = IP(raw_buffer[0:20])
             if ip_header.protocol == 'ICMP':
                 print('Protocol: %s %s -> %s' % (ip_header.protocol,ip_header.src_address, ip_header.dst_address))
