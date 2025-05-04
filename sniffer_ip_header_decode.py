22cfd3818821371606aa3e22230c91bdc8832b59
Joe <jwschurer@gmail.com>
2025-05-03 21:20:03 -0400

    added ICMP parsing

--- sniffer_ip_header_decode.py
+++ sniffer_ip_header_decode.py
@@ -32,6 +32,15 @@ class IP:
             print('%s No protocol for %s' % (e, self.protocol_num))
             self.protocol = str(self.protocol_num)
 
+class ICMP:
+    def __init__(self,buff):
+        header = struct.unpack('<BBHHH',buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
+
 def sniff(host):
     if os.name == 'nt':
         socket_protocol = socket.IPPROTO_IP
@@ -53,7 +62,20 @@ def sniff(host):
         while True:
             raw_buffer = sniffer.recvfrom(65535)[0]
             ip_header = IP(raw_buffer[0:20])
-            print('Protocol: %s %s' % (ip_header.protocol,ip_header.src_address))
+            if ip_header.protocol == 'ICMP':
+                print('Protocol: %s %s -> %s' % (ip_header.protocol,ip_header.src_address, ip_header.dst_address))
+                print(f'Version: {ip_header.ver}')
+                print(f'Header Length: {ip_header.ihl} TTL: {ip_header.ttl}')
+
+                #Calculate where icmp packet starts
+                offset = ip_header.ihl*4
+                buf = raw_buffer[offset:offset+8]
+                icmp_header = ICMP(buf)
+                print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
+
+            else:
+                print('Protocol: %s %s' % (ip_header.protocol,ip_header.src_address))
+
     except KeyboardInterrupt:
         if os.name == 'nt':
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
